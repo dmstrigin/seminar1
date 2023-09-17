0e19d3870ccaf49d10c2f571ffb0afddaa46ca91
dmstrigin <dmstrigin@yandex.ru>
2023-09-17 11:25:11 +0300

    rename lesson1 to task1

---
+++ task1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Комментарий
+Console.WriteLine("Write your number");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+// Console.WriteLine(number + " - > " + square);
+Console.WriteLine($"{number} - > {square}");
