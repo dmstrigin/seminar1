4938e40ce8843c546ffb2cdeb1b4b2c65991c811
dmstrigin <dmstrigin@yandex.ru>
2023-09-17 12:21:08 +0300

    add task 2.1

---
+++ task2.1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая будет выдавать дни недели по заданному номеру
+// 3 - > среда
+// 5 - > пятница
+
+int 1 = monday
+int 2 = thuesday
+int 3 = wednesday
+int 4 = thursday
+int 5 = friday
+
+Console.WriteLine("Enter your number");
+dayNumber = Console.ReadLine();
+if (dayNumber == "1")
+{
+    Console.WriteLine("1 - > понедельник");
+}
+else if (dayNumber == "2");
+{
+    Console.WriteLine("2 - > вторник");
+}
+else if (dayNumber == "3");
+{
+    Console.WriteLine("3 - > среда");
+}
+else if (dayNumber == "4");
+{
+    Console.WriteLine("4 - > четверг");
+}
+else if (dayNumber == "5");
+{
+    Console.WriteLine("5 - > пятница");
+}
+else if (dayNumber == "6");
+{
+    Console.WriteLine("6 - > суббота");
+}    
+else if (dayNumber == "7");
+{
+    Console.WriteLine("7 - > воскресние");
+}    
+else if (dayNumber == "8");
+{
+    Console.WriteLine("нет такого дня недели");
+}
